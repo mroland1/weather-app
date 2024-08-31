f473eb08ef592059839808d6cafd6d522e441442
Márton Roland <roland.marton227@gmail.com>
2024-08-31 10:56:21 +0200

    Search-bar

---
+++ src/components/Weather.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Weather.css";
+import search_icon from "../assets/search.png";
+
+const Weather = () => {
+  return (
+    <div className="weather">
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <img src={search_icon} alt="Search Icon" />
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
