e19cdbe9637e7db52800dc647764573ab51bab41
Márton Roland <roland.marton227@gmail.com>
2024-08-31 11:05:04 +0200

    location-temperature

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./Weather.css";
 import search_icon from "../assets/search.png";
+import clear_icon from "../assets/clear.png";
+import cloud_icon from "../assets/cloud.png";
+import drizzle_icon from "../assets/drizzle.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
+import wind_icon from "../assets/wind.png";
+import humidity_icon from "../assets/humidity.png";
 
 const Weather = () => {
   return (
@@ -9,6 +16,9 @@ const Weather = () => {
         <input type="text" placeholder="Search" />
         <img src={search_icon} alt="Search Icon" />
       </div>
+      <img src={clear_icon} alt="Clear" className="weather-icon" />
+      <p className="temperature">16&#8451;</p>
+      <p className="location">London</p>
     </div>
   );
 };
